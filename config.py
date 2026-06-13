e79b30a98b5534a019265fc9ab003bcfbe3693ea
Zennntrade <zennntrade@example.com>
2026-06-13 23:31:38 +0700

    Add Railway deployment files

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+BOT_TOKEN = "8709800059:AAFprcagqFMrISs_9flSFOc2H6gR3-3scpY"
+ADMIN_ID = 8020859688
+ADMIN_USERNAME = "zennntrade"
+COMMUNITY_CHANNEL = "https://t.me/zenthraproject"
+
+USERS_FILE = os.path.expanduser("~/zenthra_bot/database/users.json")
+SIGNALS_FILE = os.path.expanduser("~/zenthra_bot/database/signals.json")
+POSITIONS_FILE = os.path.expanduser("~/zenthra_bot/database/positions.json")
+
+MIN_VOLUME_USD = 500000
+BASE_REST = "https://fapi.binance.com"
